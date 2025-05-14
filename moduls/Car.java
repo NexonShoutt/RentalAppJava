797ef5e1c63369a75257ecd67848a1e6773efd0d
Azizah Nurul Izzah <nurulazizzaah@gmail.com>
2025-05-14 10:59:07 +0800

    Update Car.java

--- moduls/Car.java
+++ moduls/Car.java
@@ -1,13 +1,15 @@
 package moduls;
 
-// Car.java
 public class Car extends Vehicle {
-    public Car(String platNomor, String merk, int tahun) {
-        super(platNomor, merk, tahun);
+    public Car(String platNomor, String merk, int tahun, double tarifPerHari) {
+        super(platNomor, merk, tahun, tarifPerHari);
     }
 
     @Override
     public void displayInfo() {
-        System.out.println("Mobil: " + getMerk() + " | Plat: " + getPlatNomor() + " | Tahun: " + getTahun());
+        System.out.println("Mobil: " + getMerk() +
+                " | Plat: " + getPlatNomor() +
+                " | Tahun: " + getTahun() +
+                " | Tarif: Rp " + getTarifPerHari() + "/hari");
     }
 }

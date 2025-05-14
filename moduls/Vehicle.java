d2dd91025c719ab930af051397ca599c411744a4
Azizah Nurul Izzah <nurulazizzaah@gmail.com>
2025-05-14 10:58:46 +0800

    Update Vehicle.java

--- moduls/Vehicle.java
+++ moduls/Vehicle.java
@@ -1,15 +1,16 @@
 package moduls;
 
-// Vehicle.java
 public abstract class Vehicle {
     private String platNomor;
     private String merk;
     private int tahun;
+    private double tarifPerHari;
 
-    public Vehicle(String platNomor, String merk, int tahun) {
+    public Vehicle(String platNomor, String merk, int tahun, double tarifPerHari) {
         this.platNomor = platNomor;
         this.merk = merk;
         this.tahun = tahun;
+        this.tarifPerHari = tarifPerHari;
     }
 
     public String getPlatNomor() {
@@ -34,6 +35,14 @@ public abstract class Vehicle {
 
     public void setTahun(int tahun) {
         this.tahun = tahun;
+    }
+
+    public double getTarifPerHari() {
+        return tarifPerHari;
+    }
+
+    public void setTarifPerHari(double tarifPerHari) {
+        this.tarifPerHari = tarifPerHari;
     }
 
     public abstract void displayInfo();

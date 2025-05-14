cff7415503bcb45a6bcf625153eca369e5426f9c
Azizah Nurul Izzah <nurulazizzaah@gmail.com>
2025-05-14 10:34:36 +0800

    Update Motorcycle.java

--- moduls/Motorcycle.java
+++ moduls/Motorcycle.java
@@ -1,13 +1,15 @@
 package moduls;
 
-// Motorcycle.java
 public class Motorcycle extends Vehicle {
-    public Motorcycle(String platNomor, String merk, int tahun) {
-        super(platNomor, merk, tahun);
+    public Motorcycle(String platNomor, String merk, int tahun, double tarifPerHari) {
+        super(platNomor, merk, tahun, tarifPerHari);
     }
 
     @Override
     public void displayInfo() {
-        System.out.println("Motor: " + getMerk() + " | Plat: " + getPlatNomor() + " | Tahun: " + getTahun());
+        System.out.println("Motor: " + getMerk() +
+                " | Plat: " + getPlatNomor() +
+                " | Tahun: " + getTahun() +
+                " | Tarif: Rp " + getTarifPerHari() + "/hari");
     }
 }
